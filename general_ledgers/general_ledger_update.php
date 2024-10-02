7eee36280172b6bbd98b0c11b5fbe02198a75aad
NattawatTHwork <nattawatth.work@gmail.com>
2024-10-02 23:24:52 +0700

    20241002

--- general_ledgers/general_ledger_update.php
+++ general_ledgers/general_ledger_update.php
@@ -36,79 +36,160 @@
             <div class="row">
                 <div class="col-lg-12">
 
-                    <div class="card">
-                        <div class="card-body m-4">
-                            <!-- Form for general_ledger update details -->
-                            <form id="InputForm">
-                                <input type="hidden" class="form-control" id="general_ledger_id" name="general_ledger_id" required>
+                    <section class="section profile">
+                        <div class="row">
 
-                                <div class="row pb-4">
-                                    <div class="col-12">
-                                        <div class="mb-3">
-                                            <label for="document_date" class="form-label">วันที่เอกสาร</label>
-                                            <input type="date" class="form-control" id="document_date" name="document_date">
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="mb-3">
-                                            <label for="posting_date" class="form-label">วันผ่านรายการ</label>
-                                            <input type="date" class="form-control" id="posting_date" name="posting_date">
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="mb-3">
-                                            <label for="reference" class="form-label">การอ้างอิง</label>
-                                            <input type="text" class="form-control" id="reference" name="reference">
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="mb-3">
-                                            <label for="document_header_text" class="form-label">Doc.Header Text</label>
-                                            <input type="text" class="form-control" id="document_header_text" name="document_header_text">
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="mb-3">
-                                            <label for="document_type" class="form-label">ประเภทเอกสาร</label>
-                                            <input type="text" class="form-control" id="document_type" name="document_type">
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="mb-3">
-                                            <label for="intercompany_number" class="form-label">เลขที่ระหว่างบ.</label>
-                                            <input type="text" class="form-control" id="intercompany_number" name="intercompany_number">
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="mb-3">
-                                            <label for="branch_number" class="form-label">เลขที่สาขา</label>
-                                            <input type="text" class="form-control" id="branch_number" name="branch_number">
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="mb-3">
-                                            <label for="currency" class="form-label">สกุลเงิน</label>
-                                            <input type="text" class="form-control" id="currency" name="currency">
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="mb-3">
-                                            <label for="company_code" class="form-label">รหัสบริษัท</label>
-                                            <input type="text" class="form-control" id="company_code" name="company_code" disabled>
-                                        </div>
+                            <div class="col-12">
+
+                                <div class="card">
+                                    <div class="card-body pt-3">
+                                        <!-- Bordered Tabs -->
+                                        <ul class="nav nav-tabs nav-tabs-bordered">
+
+                                            <li class="nav-item">
+                                                <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#basic-data">ข้อมูลพื้นฐาน</button>
+                                            </li>
+
+                                            <li class="nav-item">
+                                                <button class="nav-link" data-bs-toggle="tab" data-bs-target="#detail">รายละเอียด</button>
+                                            </li>
+
+                                        </ul>
+                                        <div class="tab-content pt-2">
+
+                                            <div class="tab-pane fade show active basic-data" id="basic-data">
+
+                                                <form id="basic_data">
+                                                    <input type="hidden" class="form-control" id="general_ledger_id" name="general_ledger_id" required>
+
+                                                    <div class="row pb-4">
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label for="document_date" class="form-label">วันที่เอกสาร</label>
+                                                                <input type="date" class="form-control" id="document_date" name="document_date">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label for="posting_date" class="form-label">วันผ่านรายการ</label>
+                                                                <input type="date" class="form-control" id="posting_date" name="posting_date">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label for="reference" class="form-label">การอ้างอิง</label>
+                                                                <input type="text" class="form-control" id="reference" name="reference">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label for="document_header_text" class="form-label">Doc.Header Text</label>
+                                                                <input type="text" class="form-control" id="document_header_text" name="document_header_text">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label for="document_type" class="form-label">ประเภทเอกสาร</label>
+                                                                <input type="text" class="form-control" id="document_type" name="document_type">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label for="intercompany_number" class="form-label">เลขที่ระหว่างบ.</label>
+                                                                <input type="text" class="form-control" id="intercompany_number" name="intercompany_number">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label for="branch_number" class="form-label">เลขที่สาขา</label>
+                                                                <input type="text" class="form-control" id="branch_number" name="branch_number">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label for="currency" class="form-label">สกุลเงิน</label>
+                                                                <input type="text" class="form-control" id="currency" name="currency">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label for="company_code" class="form-label">รหัสบริษัท</label>
+                                                                <input type="text" class="form-control" id="company_code" name="company_code" disabled>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="row pb-4">
+                                                        <div class="col-12">
+                                                            <div class="text-center">
+                                                                <button type="submit" class="btn btn-primary w-100" id="submitBtn">บันทึก</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </form> <!-- End Form -->
+
+                                            </div>
+
+                                            <div class="tab-pane fade detail pt-3" id="detail">
+
+                                                <!-- Control Data Form -->
+                                                <form id="detail">
+                                                    <input type="hidden" class="form-control" id="gl_control_data_id" name="gl_control_data_id" required>
+
+                                                    <div class="row pb-4">
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label for="company_code" class="form-label">รหัสบริษัท</label>
+                                                                <input type="text" class="form-control" id="company_code" name="company_code">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label for="exchange_rate" class="form-label">อัตราแลกเปลี่ยน</label>
+                                                                <input type="text" class="form-control" id="exchange_rate" name="exchange_rate">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label for="translatn_date" class="form-label">วันที่ทำธุรกรรม</label>
+                                                                <input type="text" class="form-control" id="translatn_date" name="translatn_date">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <label for="trading_part_ba" class="form-label">คู่ค้าทางการค้าบัญชีธุรกิจ</label>
+                                                                <input type="text" class="form-control" id="trading_part_ba" name="trading_part_ba">
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-12">
+                                                            <div class="mb-3">
+                                                                <div class="form-check">
+                                                                    <input type="checkbox" class="form-check-input" id="calculate_tex" name="calculate_tex">
+                                                                    <label class="form-check-label" for="calculate_tex">คำนวณภาษี</label>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="row pb-4">
+                                                        <div class="col-12">
+                                                            <div class="text-center">
+                                                                <button type="submit" class="btn btn-primary w-100" id="submitBtnControlData">บันทึก</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </form><!-- End Control Data Form -->
+
+                                            </div>
+
+                                        </div><!-- End Bordered Tabs -->
+
                                     </div>
                                 </div>
 
-                                <div class="row pb-4">
-                                    <div class="col-12">
-                                        <div class="text-center">
-                                            <button type="submit" class="btn btn-primary w-100" id="submitBtn">บันทึก</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form> <!-- End Form -->
+                            </div>
                         </div>
-                    </div>
+                    </section>
 
                 </div>
 
